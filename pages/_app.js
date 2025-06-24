4256192634bf2c0ee457fcc5d73240ff77dfce57
Pedro González Fernández <pgonzalez2@esmuc.cat>
2025-06-24 12:06:43 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,6 @@
+// pages/_app.js
+import '../styles.css';           // you can delete this line later if you don’t add global CSS
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
 

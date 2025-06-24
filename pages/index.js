e75f6b0d7cace6b5d64fa7ddc417e01455a2fd52
Pedro González Fernández <pgonzalez2@esmuc.cat>
2025-06-24 12:08:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>CreativeMatch Prototype</h1>
+      <p>Select an action:</p>
+      <ul>
+        <li><Link href="/register">Register</Link></li>
+        <li><Link href="/login">Log in</Link></li>
+      </ul>
+    </main>
+  );
+}
 
